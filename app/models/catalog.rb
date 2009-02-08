154d9d1c412b9f0b9941f0168cfee493e4b4f820
AliRezaTaleghani <shayne@shayne-desktop.(none)>
2009-02-08 09:40:48 +0330

    AddUniquenessOfCatalogs

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,4 +1,6 @@
 class Catalog < ActiveRecord::Base
+   validates_uniqueness_of :department_id , :group_id, :scope => [:department_id , :group_id], :message => "This Catalog Has Been Created Before."
+   
 	belongs_to :department
 	belongs_to :group
 	has_many :posts
